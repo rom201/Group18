0d54f2758518d0da356c3e4cea96c589b2681b23
arpat9329 <68973655+arpat9329@users.noreply.github.com>
2020-09-26 19:03:30 -0400

    US#2, AC#10 done

--- src/test/java/com/assignment_5/user_story_2/User_Story_2_Test_Suit.java
+++ src/test/java/com/assignment_5/user_story_2/User_Story_2_Test_Suit.java
@@ -31,7 +31,6 @@ public class User_Story_2_Test_Suit {
 
     }
 
-
     @Test//US1_AC#5:  Users should be able to add mention by clicking on the Add mention icon and select contacts from the lists provided in dropdown.
     public void add_Mention() throws InterruptedException {
 
@@ -40,9 +39,11 @@ public class User_Story_2_Test_Suit {
         driver.findElement(By.name("USER_PASSWORD")).sendKeys("UserUser"+ Keys.ENTER);
         //US#2 Only:
         driver.findElement(By.xpath("//span[.='Task']")).click();
+        Thread.sleep(3000);
 
         //AC#5 Add mention icon and select contacts from the lists provided in dropdown
         driver.findElement(By.xpath("//div[@id='feed-add-post-content-tasks']//span[@title='Add mention']")).click();
+        Thread.sleep(2000);
         WebElement mention = driver.findElement(By.xpath("//span[@class='bx-finder-groupbox-content']//a[8]"));//a[i] -->can be more
         String expectResult = mention.getText();
         System.out.println("expectResult = " + expectResult);
@@ -54,6 +55,24 @@ public class User_Story_2_Test_Suit {
         driver.switchTo().parentFrame();
 
         Assert.assertEquals(expectResult,actuallyResult);
+    }
+
+    @Test//US1_AC#10:  Users should be able to add mention by clicking on the Add mention icon and select contacts from the lists provided in dropdown.
+    public void is_More_Visible() throws InterruptedException {
+        //beforeMethod
+        driver.findElement(By.name("USER_LOGIN")).sendKeys("helpdesk18@cybertekschool.com");
+        driver.findElement(By.name("USER_PASSWORD")).sendKeys("UserUser"+ Keys.ENTER);
+        //US#2 Only:
+        driver.findElement(By.xpath("//span[.='Task']")).click();
+        Thread.sleep(3000);
+
+        //AC#10
+        //Click "More"
+        driver.findElement(By.xpath("//div[@data-bx-id='task-edit-additional-header']")).click();
+
+        WebElement isDisplay = driver.findElement(By.xpath("//div[@data-bx-id='task-edit-unchosen-blocks']"));
+        System.out.println("isDisplay = " + isDisplay.isDisplayed());
+        Assert.assertTrue(isDisplay.isDisplayed(),"its not visible");
     }
 
 
