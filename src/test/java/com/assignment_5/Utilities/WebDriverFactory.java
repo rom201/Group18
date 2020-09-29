1b3af6bf06189b6f9808b4adcd59d8893da89c84
gulhanim <gulsariyar@gmail.com>
2020-09-29 16:44:55 -0400

    User Story 1 AC 4

---
+++ src/test/java/com/assignment_5/Utilities/WebDriverFactory.java
@@ -0,0 +1,21 @@
+package com.assignment_5.Utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebDriverFactory {
+    public static WebDriver getdriver(String browserType){
+        if(browserType.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
+        }else if(browserType.equalsIgnoreCase("firefox")){
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+        }else{
+            System.out.println("Given browser does not exist. Driver = null!");
+            return null;
+        }
+    }
+}
