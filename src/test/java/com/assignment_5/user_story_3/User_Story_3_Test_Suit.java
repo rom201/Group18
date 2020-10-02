240d6624ac3ad6a678542e87f3c12763574a4386
rom201 <kovalchukr1104@gmail.com>
2020-10-02 18:41:09 -0400

    added tests: US2_AC_1; US2_AC_7_1; US2_AC_7_1 all tests added to the test suit US2.

--- src/test/java/com/assignment_5/user_story_3/User_Story_3_Test_Suit.java
+++ src/test/java/com/assignment_5/user_story_3/User_Story_3_Test_Suit.java
@@ -106,6 +106,42 @@ public class User_Story_3_Test_Suit {
         alert.accept();
     }
 
+    @Test
+    public void us3_AC4_quote() throws InterruptedException {
+
+        // before method
+        String helpDeskUserName = "Helpdesk18@cybertekschool.com";
+        String pass = "UserUser";
+
+        WebElement userLogin = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        userLogin.sendKeys(helpDeskUserName);
+        WebElement password = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        password.sendKeys(pass);
+        WebElement loginSubmitBox = driver.findElement(By.xpath("//input[@class='login-btn']"));
+        loginSubmitBox.click();
+        Thread.sleep(1000);
+        //click on Activity Stream button
+        WebElement buttonActivityStream = driver.findElement(By.xpath("//a[@title='Activity Stream']/span[1]"));
+        buttonActivityStream.click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        // click on Event tab
+        WebElement eventTab = driver.findElement(By.xpath("//span[@id='feed-add-post-form-tab-calendar']"));
+        eventTab.click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        // actual test
+        //click on quote icon
+        WebElement quoteIcon = driver.findElement(By.xpath("//span[@id='bx-b-quote-blogPostForm_calendar']/span"));
+        quoteIcon.click();
+        // switch to iframe
+        WebDriver iframe = driver.switchTo().frame(1);
+        //find element in the text message box
+        WebElement quote = driver.findElement(By.xpath("//blockquote[@class='bxhtmled-quote']"));
+        boolean actualResult = quote.isDisplayed();
+        // verify element is displayed
+        Assert.assertTrue(actualResult);
+    }
+
     @AfterMethod
     public void afterMethod(){
         driver.manage().window().maximize();
