99346f4d384c9546c63df122481ad7bfdb6b533e
Arpat <arpat9329@gmail.com>
2020-10-02 20:47:37 -0400

    change

--- src/test/java/com/assignment_5/user_story_1/User_Story_1_AC_2.java
+++ src/test/java/com/assignment_5/user_story_1/User_Story_1_AC_2.java
@@ -1,6 +1,7 @@
 package com.assignment_5.user_story_1;
 
-import com.utilities.WebDriverFactory;
+
+import com.assignment_5.Utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -17,7 +18,7 @@ public class User_Story_1_AC_2 {
 
     @BeforeMethod
     public void set_Up(){
-        driver = WebDriverFactory.getDriver("chrome");
+        driver = WebDriverFactory.getdriver("chrome");
         driver.get("https://login2.nextbasecrm.com/");
 
     }
