7dc3a83564c999458f5d7876b42672735057e9f0
natiti100 <natalia.94@live.com>
2020-10-04 23:02:28 -0400

    User Story2_AC2

--- src/test/java/com/assignment_5/user_story_2/User_Story_2_Test_Suit.java
+++ src/test/java/com/assignment_5/user_story_2/User_Story_2_Test_Suit.java
@@ -240,6 +240,30 @@ public class User_Story_2_Test_Suit {
     }
 
 
+    @Test
+    public void link_attachment_verification() throws InterruptedException {
+
+        // driver.get("https://login2.nextbasecrm.com/");
+
+        String email = "helpdesk18@cybertekschool.com";
+        String password = "UserUser";
+
+        driver.findElement(By.xpath("//input[@name='USER_LOGIN']")).sendKeys(email);
+        driver.findElement(By.xpath("//input[@name='USER_PASSWORD']")).sendKeys(password);
+        driver.findElement(By.xpath("//input[@class='login-btn'] ")).click();
+
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[@id='feed-add-post-form-tab-tasks']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//span[@id='lhe_button_editor_task-form-lifefeed_task_form']")).click();
+
+        WebElement actualResult= driver.findElement(By.xpath("//div[@id='bx-html-editor-tlbr-idPostFormLHE_blogPostForm']"));
+        Assert.assertTrue(!actualResult.isDisplayed());
+
+        driver.manage().window().maximize();
+
+    }
+
     @AfterMethod
     public void afterMethod(){
         driver.manage().window().maximize();
