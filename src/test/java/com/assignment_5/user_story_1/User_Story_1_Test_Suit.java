e5aabd25fe6dae9811fedda801a438eb53eebeb5
Arpat <arpat9329@gmail.com>
2020-10-17 10:14:37 -0400

    US1AC9

--- src/test/java/com/assignment_5/user_story_1/User_Story_1_Test_Suit.java
+++ src/test/java/com/assignment_5/user_story_1/User_Story_1_Test_Suit.java
@@ -12,6 +12,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.util.concurrent.TimeUnit;
 
 public class User_Story_1_Test_Suit {
@@ -115,49 +116,37 @@ public class User_Story_1_Test_Suit {
 
     @Test
     public void User_Story_1_AC_9() throws InterruptedException, AWTException {
+        WebElement message = driver.findElement(By.xpath("//span[@id='feed-add-post-form-tab-message']"));
+        message.click();
+        Thread.sleep(1000);
 
-        WebDriver driver;
-        @BeforeMethod
-        public void setUpDriver() {
-            driver = WebDriverFactory.getDriver("Chrome");
-            driver.manage().window().maximize();
-            driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-        }
-        @Test
-        public void insertVideo()  AWTException {
-            driver.get("https://login2.nextbasecrm.com/");
-            driver.findElement(By.xpath("//input[@name='USER_LOGIN']")).sendKeys("helpdesk18@cybertekschool.com");
-            driver.findElement(By.xpath("//input[@name = 'USER_PASSWORD']")).sendKeys("UserUser"+ Keys.ENTER);
-            WebElement message = driver.findElement(By.xpath("//span[@id='feed-add-post-form-tab-message']"));
-            message.click();
-            Thread.sleep(1000);
-
-            WebElement recordButton = driver.findElement(By.xpath("//span[@class='feed-add-post-form-but-cnt feed-add-videomessage']"));
-            recordButton.click();
-            WebElement deviceAccess = driver.findElement(By.xpath("//div[@class='popup-window-buttons']//span[@class='popup-window-button popup-window-button-blue']"));
-            deviceAccess.click();
+        WebElement recordButton = driver.findElement(By.xpath("//span[@class='feed-add-post-form-but-cnt feed-add-videomessage']"));
+        recordButton.click();
+        WebElement deviceAccess = driver.findElement(By.xpath("//div[@class='popup-window-buttons']//span[@class='popup-window-button popup-window-button-blue']"));
+        deviceAccess.click();
 //
-            Robot robot = new Robot();
-            Thread.sleep(3000);
-            robot.keyPress(KeyEvent.VK_TAB);
-            Thread.sleep(3000);
-            robot.delay(100);
-            Thread.sleep(3000);
-            robot.keyPress(KeyEvent.VK_TAB);
-            Thread.sleep(3000);
-            robot.delay(100);
-            Thread.sleep(3000);
-            robot.keyPress(KeyEvent.VK_ENTER);
-            Thread.sleep(3000);
-            Thread.sleep(2000);
+        Robot robot = new Robot();
+        Thread.sleep(3000);
+        robot.keyPress(KeyEvent.VK_TAB);
+        Thread.sleep(3000);
+        robot.delay(100);
+        Thread.sleep(3000);
+        robot.keyPress(KeyEvent.VK_TAB);
+        Thread.sleep(3000);
+        robot.delay(100);
+        Thread.sleep(3000);
+        robot.keyPress(KeyEvent.VK_ENTER);
+        Thread.sleep(3000);
+        Thread.sleep(2000);
 
-            WebElement errorText = driver.findElement(By.xpath("//span[@class='popup-window-button popup-window-button-blue']"));
-            errorText.click();
+        WebElement errorText = driver.findElement(By.xpath("//span[@class='popup-window-button popup-window-button-blue']"));
+        errorText.click();
 
-
+    }
     @AfterMethod
-    public void afterMethod(){
+    public void afterMethod() {
         driver.manage().window().maximize();
         driver.close();
     }
+
 }
